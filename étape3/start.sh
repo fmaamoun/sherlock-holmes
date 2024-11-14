d5b43177b48db627fff9851bc95e270b79e76258
Maamoun Firas <firas.maamoun@gmail.com>
2024-11-14 18:45:30 +0100

    Update start.sh

--- étape3/start.sh
+++ étape3/start.sh
@@ -2,6 +2,10 @@
 
 echo "Préparation de l'étape 3..."
 
+# Supprimer tous les fichiers et dossiers sauf "verif.sh" et "start.sh"
+find . -maxdepth 1 ! -name 'verif.sh' ! -name 'start.sh' ! -name '.' -exec rm -rf {} +
+
+# Unset la variable env COUPABLE
 unset COUPABLE
 
 echo "Les fichiers nécessaires ont été mis en place."
